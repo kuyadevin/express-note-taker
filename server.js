6347206f76722529b707d68229f0469602141ce2
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-28 15:58:46 -0700

    Added middleware for parsing to server.js

--- server.js
+++ server.js
@@ -8,21 +8,28 @@ const app = express();
 //Set port
 const PORT = process.env.PORT || 3001;
 
-// Route to get notes
+// Middleware for parsing application/json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Route to notes.html 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
-
-app.get('api/notes', (req, res) => res.json(notesData));
-
-app.listen(PORT, () => {
-  console.log('App listening at http://localhost:' + PORT)
 });
 
 // Route to index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
 });
+
+//Reading db file and saving notes as JSON
+app.get('api/notes', (req, res) => res.json(notesData));
+
+// Post Request
+app.post('api/notes', (req, res) => {
+  
+});
+
 
 app.listen(PORT, () => {
   console.log('App listening at http://localhost:' + PORT)
