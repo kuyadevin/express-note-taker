598352b00846006fdb6995be765f60e6b78d7573
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-28 15:28:29 -0700

    added port const and node modules to server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+//Require node modules
 const express = require('express');
 const path = require('path');
+
+const app = express();
+//Set port
+const PORT = process.env.PORT || 3001;
+
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
