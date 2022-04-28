ed785820e2e395e38c8f0f65aa3242bab01e4dd5
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-28 15:34:40 -0700

    added inital get function in server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 //Require node modules
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const notesData = require('./db/db.json');
 
 const app = express();
 //Set port
 const PORT = process.env.PORT || 3001;
 
 
-app.get('/notes', (req, res) =>
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+});
+
+app.get('api/notes', (req, res) => res.json(notesData));
