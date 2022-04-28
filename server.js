1b35bd17c1971cdb34d13374204948a7fc10b7e8
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-28 15:40:00 -0700

    added app.listen to the get route on server.js

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const app = express();
 //Set port
 const PORT = process.env.PORT || 3001;
 
-
+// Route to get notes
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 app.get('api/notes', (req, res) => res.json(notesData));
+
+app.listen(PORT, () => {
+  console.log('App listening at http://localhost:' + PORT)
+});
