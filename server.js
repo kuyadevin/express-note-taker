6ac5e538ba112ef7f14896dfa87f49c972f539ed
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-28 15:42:54 -0700

    added route to index.html on server.js

--- server.js
+++ server.js
@@ -18,3 +18,12 @@ app.get('api/notes', (req, res) => res.json(notesData));
 app.listen(PORT, () => {
   console.log('App listening at http://localhost:' + PORT)
 });
+
+// Route to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.listen(PORT, () => {
+  console.log('App listening at http://localhost:' + PORT)
+});
